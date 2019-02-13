abbaebf4676f605181614e9dd44771cb3b21e9ac
JOHN\john <forza11879@gmail.com>
2019-02-13 01:49:42 -0500

    update error handling

--- controllers/notFound.js
+++ controllers/notFound.js
@@ -1,14 +1,13 @@
-exports.notFoundPage= (req, res) => {
+exports.notFoundPage = (req, res) => {
+  res.status(404).render('404')
+  // res.status(404).render('404', {
+  //   // res.render('main.handlebars', {
+  //   // res.render('index.html', {
+  //   // pageTitle: 'Home Page by '
+  //   // nameUpperCase: req.query.name.toUpperCase(),
+  //   // name: req.query.name,
+  //   // salesEnd: moment().endOf('day').fromNow()
 
-
-
-  res.render('404', {
-    // res.render('main.handlebars', {
-    // res.render('index.html', {
-    // pageTitle: 'Home Page by '
-    // nameUpperCase: req.query.name.toUpperCase(),
-    // name: req.query.name,
-    // salesEnd: moment().endOf('day').fromNow()
-
-  })
+  // })
 }
+
