544e9c0964b9f14195214f4d0e096010af9de1d9
JOHN\john <forza11879@gmail.com>
2019-03-13 17:55:37 -0400

    separation of concerns done!

--- controllers/webApiListController.js
+++ controllers/webApiListController.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
-const { List } = require('../db/models/List')
-// const db = require('../db')
+// const { List } = require('../db/models/List')
+const db = require('../db')
 // const { saveToDb,generateUrlArray,fetchDataFromDb } = require('../d/index')
 
 
@@ -34,83 +34,83 @@ exports.getWebApiList = (req, res) => {
     }
   }
 
-  async function saveToDb(arg) {
-    try {
-      const stockList = new List({
-        symbol: arg.symbol,
-        open: arg.open,
-        high: arg.high,
-        low: arg.low,
-        price: arg.price,
-        volume: arg.volume,
-        latestTrdDay: arg.latestTrdDay,
-        previousClose: arg.previousClose,
-        change: arg.change,
-        changePercent: arg.changePercent
-      })
+  // async function saveToDb(arg) {
+  //   try {
+  //     const stockList = new List({
+  //       symbol: arg.symbol,
+  //       open: arg.open,
+  //       high: arg.high,
+  //       low: arg.low,
+  //       price: arg.price,
+  //       volume: arg.volume,
+  //       latestTrdDay: arg.latestTrdDay,
+  //       previousClose: arg.previousClose,
+  //       change: arg.change,
+  //       changePercent: arg.changePercent
+  //     })
 
-      const query = { symbol: `${stockList.symbol}` }
-      const update = {
-        open: stockList.open,
-        high: stockList.high,
-        low: stockList.low,
-        price: stockList.price,
-        volume: stockList.volume,
-        latestTrdDay: stockList.latestTrdDay,
-        previousClose: stockList.previousClose,
-        change: stockList.change,
-        changePercent: stockList.changePercent
-      }
-      // new: bool - if true, return the modified document rather than the original. defaults to false (changed in 4.0)
-      // upsert: bool - creates the object if it doesn't exist. defaults to false.
-      const options = { upsert: true, new: true }
+  //     const query = { symbol: `${stockList.symbol}` }
+  //     const update = {
+  //       open: stockList.open,
+  //       high: stockList.high,
+  //       low: stockList.low,
+  //       price: stockList.price,
+  //       volume: stockList.volume,
+  //       latestTrdDay: stockList.latestTrdDay,
+  //       previousClose: stockList.previousClose,
+  //       change: stockList.change,
+  //       changePercent: stockList.changePercent
+  //     }
+  //     // new: bool - if true, return the modified document rather than the original. defaults to false (changed in 4.0)
+  //     // upsert: bool - creates the object if it doesn't exist. defaults to false.
+  //     const options = { upsert: true, new: true }
 
-      const stockResult = await List.findOneAndUpdate(query, update, options)
-      console.log('Saved the symbol web TO dbList', stockResult.symbol)
-    } catch (ex) {
-      console.log(`saveToDb error: ${ex}`)
-    }
-  }
+  //     const stockResult = await List.findOneAndUpdate(query, update, options)
+  //     console.log('Saved the symbol web TO dbList', stockResult.symbol)
+  //   } catch (ex) {
+  //     console.log(`saveToDb error: ${ex}`)
+  //   }
+  // }
 
-  async function fetchDataFromDb() {
-    try {
-      const query = null
-      const projection = { _id: 0 }
-      return dataFromDB = await List.find(query, projection).then(item => {
-        return item.map(item => {
-          return {
-            symbol: item.symbol, //symbol
-            open: parseFloat(item.open), // the open
-            high: parseFloat(item.high), // high
-            low: parseFloat(item.low), // low
-            price: parseFloat(item.price), // price
-            volume: parseFloat(item.volume), // volume
-            latestTrdDay: new Date(parseFloat(item.latestTrdDay)).toDateString(),//latestTrdDay
-            previousClose: parseFloat(item.previousClose),//previousClose
-            change: parseFloat(item.change),
-            changePercent: parseFloat(item.changePercent)//previousClose
-          }
-        })
-      })
+  // async function fetchDataFromDb() {
+  //   try {
+  //     const query = null
+  //     const projection = { _id: 0 }
+  //     return dataFromDB = await List.find(query, projection).then(item => {
+  //       return item.map(item => {
+  //         return {
+  //           symbol: item.symbol, //symbol
+  //           open: parseFloat(item.open), // the open
+  //           high: parseFloat(item.high), // high
+  //           low: parseFloat(item.low), // low
+  //           price: parseFloat(item.price), // price
+  //           volume: parseFloat(item.volume), // volume
+  //           latestTrdDay: new Date(parseFloat(item.latestTrdDay)).toDateString(),//latestTrdDay
+  //           previousClose: parseFloat(item.previousClose),//previousClose
+  //           change: parseFloat(item.change),
+  //           changePercent: parseFloat(item.changePercent)//previousClose
+  //         }
+  //       })
+  //     })
 
-      // .select('symbol open high low price volume latestTrdDay previousClose change changePercent')
-      // .populate('open high low price volume latestTrdDay previousClose change changePercent')
-    } catch (ex) {
-      console.log(`fetchDataFromDb error: ${ex}`)
-    }
-  }
-  async function generateUrlArray() {
-    try {
-      const query = {}
-      const projection = { _id: 0 }
-      const dataFromDB = await List.find(query, projection).select('symbol')
-      return linkArray = dataFromDB.map(item => {
-        return link = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${item.symbol}&apikey=6BUYSS9QR8Y9HH15`
-      })
-    } catch (ex) {
-      console.log(`generateUrlArray error: ${ex}`)
-    }
-  }
+  //     // .select('symbol open high low price volume latestTrdDay previousClose change changePercent')
+  //     // .populate('open high low price volume latestTrdDay previousClose change changePercent')
+  //   } catch (ex) {
+  //     console.log(`fetchDataFromDb error: ${ex}`)
+  //   }
+  // }
+  // async function generateUrlArray() {
+  //   try {
+  //     const query = {}
+  //     const projection = { _id: 0 }
+  //     const dataFromDB = await List.find(query, projection).select('symbol')
+  //     return linkArray = dataFromDB.map(item => {
+  //       return link = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${item.symbol}&apikey=6BUYSS9QR8Y9HH15`
+  //     })
+  //   } catch (ex) {
+  //     console.log(`generateUrlArray error: ${ex}`)
+  //   }
+  // }
 
   // async function loopSaveToDb(newArray) {
   //   for (const url of newArray) {
@@ -122,7 +122,7 @@ exports.getWebApiList = (req, res) => {
   async function webApiSaveToDb(url) {
     try {
       const data = await fetchWebApiList(url)
-      await saveToDb(data)
+      await db.saveToDb(data)
     } catch (ex) {
       console.log(`loopSaveToDb error: ${ex}`)
     }
@@ -131,8 +131,8 @@ exports.getWebApiList = (req, res) => {
 
   (async function fetchDataList() {
     try {
-      // const urlArray = await db.generateUrlArray({}, { _id: 0 })
-      const urlArray = await generateUrlArray()
+      const urlArray = await db.generateUrlArray({}, { _id: 0 })
+      // const urlArray = await db.generateUrlArray()
       const promises = []
 
       // for (i = 0; i < urlArray.length; ++i) {
@@ -154,8 +154,7 @@ exports.getWebApiList = (req, res) => {
         await Promise.all(promises)
       }
 
-      // const dataFromDB = await db.fetchDataFromDb({}, { _id: 0 })
-      const dataFromDB = await fetchDataFromDb()
+      const dataFromDB = await db.fetchDataFromDb({}, { _id: 0 })
       return res.send(dataFromDB)
     } catch (ex) {
       console.log(`fetchDataList error: ${ex}`)
