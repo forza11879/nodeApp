ccfd183a2dbb8cf2fe473b4bcd49f99a6fec4152
JOHN\john <forza11879@gmail.com>
2019-04-03 02:01:08 -0400

    db upadte

--- startup/db.js
+++ startup/db.js
@@ -4,9 +4,9 @@ module.exports = function () {
   mongoose.Promise = global.Promise // Tell Mongoose to use ES6 promises
   // Connect to our Database and handle any bad connections
 
-  mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true })
+  // mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true })
 
-  // mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ckbfd.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true`, { useNewUrlParser: true })
+  mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ckbfd.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true`, { useNewUrlParser: true })
 
   //(node:571) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
   mongoose.set('useCreateIndex', true)
