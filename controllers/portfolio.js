1d4fa0c3af5da8e3f6481ae7d78f17ed56fd6fac
JOHN\john <forza11879@gmail.com>
2019-04-05 02:34:10 -0400

    working on buysell ticket details

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -4,9 +4,8 @@ exports.getBuySellTicket = async (req, res) => {
   const curValue = req.params.symbol
   const apiTokenQuote = process.env.API_TOKEN_QUOTE
   const url = `https://cloud.iexapis.com/beta/stock/${curValue}/quote?token=${apiTokenQuote}`
-  // const url = `https://cloud.iexapis.com/beta/stock/aapl/quote?token=pk_0f5e9e6bca7548918adc6512bcb57ff4`
   const data = await db.fetchWebApiQuote(url)
-  console.log(data)
+  // console.log(data)
   res.render('buysell', {
     curValue: curValue,
     data: data
--- db/models/Portfolio/index.js
+++ db/models/Portfolio/index.js
@@ -1,4 +1,6 @@
 const axios = require('axios')
+const moment = require('moment')
+
 const { Portfolio } = require('./Portfolio')
 
 const fetchWebApiQuote = async url => {
@@ -6,15 +8,17 @@ const fetchWebApiQuote = async url => {
     const myJson = await axios.get(url)
     const myJsonData = myJson.data
     return {
-      symbol:myJsonData['symbol'],
-      latestPrice:myJsonData['latestPrice'],
-      high:myJsonData['high'],
-      low:myJsonData['low'],
-      week52High:myJsonData['week52High'],
-      week52Low:myJsonData['week52Low'],
-      open:myJsonData['open'],
-      previousClose:myJsonData['previousClose']
-      
+      symbol: myJsonData['symbol'],
+      companyName: myJsonData['companyName'],
+      latestPrice: myJsonData['latestPrice'],
+      change:myJsonData['change'],
+      latestUpdate: moment(myJsonData['latestUpdate']).format('LLLL'),
+      high: myJsonData['high'],
+      low: myJsonData['low'],
+      week52High: myJsonData['week52High'],
+      week52Low: myJsonData['week52Low'],
+      open: myJsonData['open'],
+      previousClose: myJsonData['previousClose']
     }
   } catch (ex) {
     console.log(`fetchWebApiQuote error: ${ex}`)
