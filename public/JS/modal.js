6ca274a470edbf1056d37c3b902c48681de1a180
JOHN\john <forza11879@gmail.com>
2019-04-09 16:48:07 -0400

    working input value

--- public/JS/modal.js
+++ public/JS/modal.js
@@ -1,7 +1,7 @@
-const qtyBidValue = document.querySelector("#qtyBid").value
-const buyTicketValue = document.querySelector("#buyTicket").value
-const askPriceValue = document.querySelector("#askPrice").value
-const nameSymbolValue = document.querySelector("#nameSymbol").value
+// const qtyAskValue = document.querySelector("#qtyAsk").value.trim()
+const buyTicket = document.querySelector("#buyTicket")
+// const askPriceValue = document.querySelector("#askPrice").value
+// const nameSymbolValue = document.querySelector("#nameSymbol").value
 
 buyTicket.addEventListener("click", getModalInfo)
 
@@ -9,31 +9,49 @@ buyTicket.addEventListener("click", getModalInfo)
 class UI {
   constructor() {
     this.show = document.querySelector("#modalContent")
+    this.qtyAskValue = document.querySelector("#qtyAsk").value.trim()
+    
   }
-  showData(qtyBidValue) {
+  showData() {
     // console.log(data)
     this.show.innerHTML = `<div class="modal-header">
-    <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
-    </div>
-    <div class="modal-body">
-    You are placing an order for ${qtyBidValue} shares of  at 
-    </div>
-    <div class="modal-footer">
-    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-primary">Save changes</button>
-    </div>`
-
+      <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>
+      <div class="modal-body">
+      <p>You are placing an order for ${this.qtyAskValue} shares of  at</p>
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-primary">Save changes</button>
+      </div>`
   }
 }
 const ui = new UI()
 //Model
 function getModalInfo() {
-  ui.showData(qtyBidValue)
+  ui.showData()
 }
 
+// function getModalInfo() {
+//   getTradingValues()
+//     .then(data => {
+//       console.log(data)
+//       ui.showData(data)
+//     })
+//     .catch(error => console.error("Error:", error))
+// }
+
+// function getTradingValues() {
+//   return {
+//     qtyBidValue: document.querySelector("#qtyBid").value.trim()
+//     // buyTicketValue: document.querySelector("#buyTicket").value,
+//     // askPriceValue: document.querySelector("#askPrice").value,
+//     // nameSymbolValue: document.querySelector("#nameSymbol").value
+//   }
+// }
 
 // const ui = new UI()
 // //Model
