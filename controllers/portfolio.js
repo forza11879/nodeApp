6e1774b66bffaaa2284b6b15ee7aaea6c2a5d2de
JOHN\john <forza11879@gmail.com>
2019-04-12 20:16:34 -0400

    edits

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,10 +1,14 @@
 const db = require('../db/models/Portfolio')
+const user = require('../db/models/User')
 
 exports.getBuySellTicket = async (req, res) => {
   const curValue = req.params.symbol
   const apiTokenQuote = process.env.API_TOKEN_QUOTE
   const url = `https://cloud.iexapis.com/beta/stock/${curValue}/quote?token=${apiTokenQuote}`
+
   const data = await db.fetchWebApiQuote(url)
+
+  
   res.render('buysell', {
     data: data
   })
