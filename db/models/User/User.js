442a26ef83e096f77b4f928bca17e492c358d4b0
JOHN\john <forza11879@gmail.com>
2019-05-02 03:45:17 -0400

    added reset token to User model

--- db/models/User/User.js
+++ db/models/User/User.js
@@ -3,6 +3,8 @@ const ParentSchemaSymbolList = new mongoose.Schema({
   email: String,
   name: String,
   password: String,
+  resetToken: String,
+  resetTokenExperation: Date,
   cash: {
     type: mongoose.Types.Decimal128,
     default: 50000.0
