49757d1314d68ac62ef3fbde7a657e2a8ea8ecda
JOHN\john <forza11879@gmail.com>
2019-05-06 16:40:32 -0400

    fetching user data

--- db/models/User/index.js
+++ db/models/User/index.js
@@ -4,12 +4,13 @@ const fetchUserDataDB = async userId => {
   try {
     const query = { _id: userId }; //Optional. Specifies selection filter using query operators. To return all documents in a collection, omit this parameter or pass an empty document ({}).
 
-    const projection = { _id: 0 }; //	Optional. Specifies the fields to return in the documents that match the query filter. To return all fields in the matching documents, omit this parameter. For details, see Projection.
+    const projection = { _id: 1 }; //	Optional. Specifies the fields to return in the documents that match the query filter. To return all fields in the matching documents, omit this parameter. For details, see Projection.
 
-    const user = await User.findOne(query, projection).select(
-      'name cash equity -_id'
+    const user = await User.findById(query, projection).select(
+      '_id name cash equity'
     ); //findOne() returns the Object{} without the Array vs find() Array[{}] of Objects
     return {
+      id: user._id,
       name: user.name,
       cash: parseFloat(user.cash),
       equity: parseFloat(user.equity)
