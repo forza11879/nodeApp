8402f4cf72751cb03b662a1a573ac5757e802054
Ion Leahu <forza11879@gmail.com>
2019-11-06 00:23:29 -0500

    fixed PortfolioList

--- public/JS/fetchPortfolioList.js
+++ public/JS/fetchPortfolioList.js
@@ -19,29 +19,16 @@ class UI {
   showData(portfolio) {
     // const userId = '5d5f6afb11a620047486274d';
     // const userId = req.session.use._id;
-    // console.log(portfolio);
+    console.log(portfolio);
     this.show.innerHTML = portfolio
       .map(item => {
-        let sellPrice = parseFloat(
-          item.symbolDb[0].transactionDb[0].totalBuySellTradeAmount
-            .$numberDecimal
-        );
-        let buyPrice = parseFloat(
-          item.symbolDb[0].transactionDb[1].totalBuySellTradeAmount
-            .$numberDecimal
-        );
-        let avrPrice = (buyPrice - sellPrice) / parseInt(item.qtyPortfolio);
-        // console.log(`sellPrice: ${typeof sellPrice}`);
-        // console.log(`sellPrice: ${JSON.stringify(sellPrice)}`);
-        // console.log(`buyPrice: ${typeof buyPrice}`);
-        // console.log(`buyPrice: ${JSON.stringify(buyPrice)}`);
-        // console.log(`avrPrice: ${typeof avrPrice}`);
-        // console.log(`avrPrice: ${JSON.stringify(avrPrice)}`);
+        // console.log(`avgPrice: ${typeof item.avgPrice}`);
+        // console.log(`avgPrice: ${JSON.stringify(item.avgPrice)}`);
         return `<tr>
             <td>${item.symbolDb[0].symbol}</td>
             <td>${item.symbolDb[0].data[0].close.$numberDecimal}</td>
             <td>${item.qtyPortfolio}</td>
-            <td>${avrPrice.toFixed(2)}</td>
+            <td>${item.avgPrice.toFixed(2)}</td>
             <td>${item.symbolDb[0].symbol}</td>
          </tr>`;
       })
